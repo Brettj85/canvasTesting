0fb687a779c3279a759049a4e9cdca937925def4
brett <brettj85@gmail.com>
2018-10-29 19:41:24 -0500

    finished some bouncing colored circles

--- canvas.js
+++ canvas.js
@@ -51,7 +51,27 @@ var c = canvas.getContext('2d');
 // c.stroke();
 
 //random
+var mouse = {
+    x: undefined,
+    y: undefined
+}
 
+var maxRadius = 40;
+var minRadius;
+
+var colorArray = [
+    'rgba(255, 0, 0, .8)',
+    'rgba(0, 255, 0, .8)',
+    'rgba(0, 0, 255, .8)',
+    'rgba(255, 255, 0, .8)',
+    'rgba(0, 255, 255, .8)'
+]
+
+window.addEventListener('mousemove', (event) => {
+    mouse.x = event.x;
+    mouse.y = event.y;
+    console.log(mouse);
+})
 
 function Circle(x, y, dx, dy, radius) {
     this.x = x;
@@ -59,15 +79,18 @@ function Circle(x, y, dx, dy, radius) {
     this.dx = dx;
     this.dy = dy;
     this.radius = radius;
+    this.minRadius = radius;
+    this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
+
     this.draw = function() {
-        c.clearRect(0,0, innerWidth, innerHeight);
         c.beginPath();
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         c.strokeStyle = 'blue';
-        c.stroke();
+        c.fillStyle = this.color;
+        c.fill();
     }
 
-    this.update = function() {
+    this.update = () => {
         if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
             this.dx = -this.dx;
         }
@@ -77,17 +100,27 @@ function Circle(x, y, dx, dy, radius) {
         this.x += this.dx;
         this.y += this.dy;
 
+        //interactivity
+        if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50){
+            if (this.radius < maxRadius){
+                this.radius += 1;
+            }
+        } else if (this.radius > this.minRadius) {
+            this.radius -= 1;
+        }
+
         this.draw();
     }
 }
 
 var circleArray = [];
-for (var i = 0; i < 100; i++) {
-    var x = Math.random() * window.innerWidth;
-    var y = Math.random() * window.innerHeight;
+
+for (var i = 0; i < 200; i++) {
+    var radius = Math.random() * 10 + 1;
+    var x = Math.random() * (window.innerWidth - radius * 2) + radius;
+    var y = Math.random() * (window.innerHeight - radius * 2) + radius;
     var dx = (Math.random() - 0.5) * 5;
     var dy = (Math.random() - 0.5) * 5;
-    var radius = 30;
     circleArray.push(new Circle(x, y, dx, dy, radius));
 }
 
